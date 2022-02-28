9faf40154323693ec9f1f54d6495a01731bd8952
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-02-28 17:28:59 +0200

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -6,6 +6,6 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt full-upgrade
-sudo apt install gnome-tweaks thunderbird lutris spotify-client code ubuntu-restricted-extras nano neofetch gedit gedit-plugin-text-size gnome-tweak-too spell -y
+sudo apt install gnome-tweaks thunderbird lutris spotify-client code ubuntu-restricted-extras nano neofetch gedit gedit-plugin-text-size gnome-tweaks spell -y
 sudo apt purge gnome-software -y
 sudo sanp remove snap-store
